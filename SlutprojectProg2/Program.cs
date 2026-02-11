d264363785da241a67fd32f6261535a978757bec
Anton Faren <Anton.faren@elev.ga.ntig.se>
2026-02-11 13:53:39 +0100

    first commit

---
+++ SlutprojectProg2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+
+    Raylib.InitWindow(800, 800, "game");
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose())
+    {   
+        
+        BeginDrawing();
+        ClearBackground(Color.Black);
+        DrawText("hej",200,200,200,Color.White);
+
+        DrawCircle(475,225,20,Color.White);
+        EndDrawing();
+
+    }
+
+
+
+
+
+
+
+
+
